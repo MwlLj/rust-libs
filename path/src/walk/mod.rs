0d527cd7fa34930f3f31bb740b88655ace96da06
MwlLj <731025894@qq.com>
2019-09-27 13:14:42 +0800

    提供回调的方法

--- path/src/walk/mod.rs
+++ path/src/walk/mod.rs
@@ -5,11 +5,11 @@ pub trait IWalk {
     fn on_file(&mut self, path: &str, name: &str);
 }
 
-struct CWalk {
+pub struct CWalk {
 }
 
 impl CWalk {
-    fn walk<F: IWalk>(&self, root: &str, f: &mut F) -> Result<(), &str> {
+    pub fn walk<F: IWalk>(&self, root: &str, f: &mut F) -> Result<(), &str> {
         let p = path::Path::new(root);
         let dirs = match p.read_dir() {
             Ok(d) => d,
@@ -66,6 +66,37 @@ impl CWalk {
     pub fn new() -> CWalk {
         CWalk{}
     }
+}
+
+struct CDefault<'b, DirF: FnMut(&str, &str), FileF: FnMut(&str, &str)> {
+    dirF: &'b mut DirF,
+    fileF: &'b mut FileF
+}
+
+impl<'b, DirF, FileF> IWalk for CDefault<'b, DirF, FileF>
+    where DirF: FnMut(&str, &str)
+    , FileF: FnMut(&str, &str) {
+    fn on_dir(&mut self, path: &str, name: &str) {
+        (self.dirF)(path, name);
+    }
+
+    fn on_file(&mut self, path: &str, name: &str) {
+        (self.fileF)(path, name);
+    }
+}
+
+pub fn walk<'a, DirF, FileF>(root: &'a str, dirF: &mut DirF, fileF: &mut FileF) -> Result<(), &'a str>
+    where DirF: FnMut(&str, &str)
+    , FileF: FnMut(&str, &str) {
+    let mut default = CDefault{
+        dirF: dirF,
+        fileF: fileF
+    };
+    let walker = CWalk::new();
+    if let Err(err) = walker.walk(root, &mut default) {
+        return Err("walk error");
+    };
+    Ok(())
 }
 
 #[cfg(test)]
